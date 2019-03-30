cca1d6824c6a7fffc5c5b149f7f4be37361393cc
Luis Machado <Luis.Machado@live.com.pt>
2019-03-30 17:33:14 +0000

    fix

--- Week_13/async_code1/app.js
+++ Week_13/async_code1/app.js
@@ -5,3 +5,8 @@ fs.writeFile('/file.js', '//' + fileContent, (err) => {
     if (err) return console.log(err);
     console.log("File 1 saved!");
 })
+
+fs.writeFile('/file.html', '//' + fileContent, (err) => {
+    if (err) return console.log(err);
+    console.log("File 2 saved!");
+})
